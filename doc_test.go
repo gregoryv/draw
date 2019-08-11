fc120da69335de92a150c13cf0a45a83c9094f7c
Gregory Vincic <g@7de.se>
2019-08-11 09:12:23 +0200

    rm golden test for the result

--- doc_test.go
+++ doc_test.go
@@ -1,33 +1,26 @@
 package design
 
 import (
-	"io/ioutil"
 	"testing"
-
-	"github.com/gregoryv/golden"
 )
 
 func Test_render_design_document(t *testing.T) {
+	doc := NewReferenceDoc()
+	filename := "result.html"
+	doc.SaveAs(filename)
+}
+
+func NewReferenceDoc() *DesignDoc {
 	doc := NewDesignDoc()
 	write := doc.Editor()
-	write("<h1>Design Document as Software</h1>")
+	write(
+		"<h1>Go-Design Diagram Reference</h1>",
+	)
 
 	graph := NewGraph()
 	graph.Title = "Struct component"
 	graph.NewComponent(Account{})
 
 	write(graph)
-
-	filename := "result.html"
-	doc.SaveAs(filename)
-	golden.Assert(t, loadFile(t, filename))
-}
-
-func loadFile(t *testing.T, filename string) string {
-	t.Helper()
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return string(body)
+	return doc
 }
