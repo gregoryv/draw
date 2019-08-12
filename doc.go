693cef1b375c11edcca498ab0c5863f0c1110b76
Gregory Vincic <g@7de.se>
2019-08-12 06:34:30 +0200

    Rename Document

--- doc.go
+++ doc.go
@@ -7,28 +7,28 @@ import (
 	"text/template"
 )
 
-type DesignDoc struct {
+type Document struct {
 	Parts Stringers
 	Style Stringer
 }
 
-func NewDesignDoc() *DesignDoc {
-	return &DesignDoc{}
+func NewDesignDoc() *Document {
+	return &Document{}
 }
 
-func (doc *DesignDoc) Editor() Editor {
+func (doc *Document) Editor() Editor {
 	return doc.edit
 }
 
 type Editor func(...interface{})
 
-func (doc *DesignDoc) edit(arguments ...interface{}) {
+func (doc *Document) edit(arguments ...interface{}) {
 	for _, arg := range arguments {
 		doc.appendByType(arg)
 	}
 }
 
-func (doc *DesignDoc) appendByType(arg interface{}) {
+func (doc *Document) appendByType(arg interface{}) {
 	var valid Stringer
 	switch a := arg.(type) {
 	case string:
@@ -44,7 +44,7 @@ func (doc *DesignDoc) appendByType(arg interface{}) {
 	doc.Parts = append(doc.Parts, valid)
 }
 
-func (doc *DesignDoc) SaveAs(filename string) error {
+func (doc *Document) SaveAs(filename string) error {
 	fh, err := os.Create(filename)
 	if err != nil {
 		return err
@@ -54,7 +54,7 @@ func (doc *DesignDoc) SaveAs(filename string) error {
 	return err
 }
 
-func (doc *DesignDoc) WriteTo(w io.Writer) (int, error) {
+func (doc *Document) WriteTo(w io.Writer) (int, error) {
 	r, writer := io.Pipe()
 	go func() {
 		tpl := template.Must(template.New("html").Parse(htmlSource))
