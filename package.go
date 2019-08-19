6529581fa19c28cc9c52e15749f63cffa37d7a6f
Gregory Vincic <g@7de.se>
2019-08-19 21:52:33 +0200

    Use WriteDefault

--- package.go
+++ package.go
@@ -7,16 +7,17 @@ import (
 	"github.com/gregoryv/go-design/shape"
 )
 
-// WriteDefault applies default style while writing diagram to w
-func WriteDefault(dia shape.SvgWriter, w io.Writer) error {
-	return dia.WriteSvg(NewStyler(w))
-}
-
 // saveAs saves diagram using default style to filename
 func saveAs(dia shape.SvgWriter, filename string) error {
 	fh, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
-	return dia.WriteSvg(NewStyler(fh))
+	defer fh.Close()
+	return WriteDefault(dia, fh)
+}
+
+// WriteDefault applies default style while writing diagram to w
+func WriteDefault(dia shape.SvgWriter, w io.Writer) error {
+	return dia.WriteSvg(NewStyler(w))
 }
