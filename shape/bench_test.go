2cf1ab315124913a78ed722b54ee46562950f555
Gregory Vincic <g@7de.se>
2019-09-09 20:44:40 +0200

    Move benchmark test into shape package

--- shape/bench_test.go
+++ shape/bench_test.go
@@ -1,22 +1,21 @@
-package design
+package shape
 
 import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/gregoryv/go-design/shape"
 	"github.com/gregoryv/go-design/style"
 )
 
 func BenchmarkWriteSvg(b *testing.B) {
-	diagram := &shape.Svg{
-		Content: []shape.SvgWriterShape{
-			&shape.Record{},
+	svg := &Svg{
+		Content: []SvgWriterShape{
+			&Record{},
 		},
 	}
 	styler := style.NewStyler(ioutil.Discard)
 	for i := 0; i < b.N; i++ {
-		diagram.WriteSvg(styler)
+		svg.WriteSvg(styler)
 	}
 	b.StopTimer()
 	b.ReportAllocs()
