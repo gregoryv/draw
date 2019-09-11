16d72ffd295d19ebf884a06610531122c2767880
Gregory Vincic <g@7de.se>
2019-09-11 06:50:13 +0200

    Test line

--- shape/line_test.go
+++ shape/line_test.go
@@ -10,6 +10,11 @@ import (
 func Test_one_line(t *testing.T) {
 	it := &one_line{t, NewLine(1, 1, 7, 7)}
 	it.can_be_rendered_as_svg()
+	it.s_directed_right()
+
+	it = &one_line{t, NewLine(8, 8, 0, 0)}
+	it.s_directed_left()
+	it.can_move()
 }
 
 type one_line struct {
@@ -23,4 +28,30 @@ func (t *one_line) can_be_rendered_as_svg() {
 	t.WriteSvg(buf)
 	assert := asserter.New(t)
 	assert().Contains(buf.String(), "<line ")
+}
+
+func (t *one_line) s_directed_right() {
+	t.Helper()
+	dir := t.Direction()
+	if dir != LR {
+		t.Error("not directed right")
+	}
+}
+
+func (t *one_line) s_directed_left() {
+	t.Helper()
+	dir := t.Direction()
+	if dir != RL {
+		t.Error("not directed left")
+	}
+}
+
+func (t *one_line) can_move() {
+	t.Helper()
+	s, e := t.Start, t.End
+	t.SetX(s.X + 1)
+	t.SetY(s.Y + 1)
+	assert := asserter.New(t)
+	assert(!t.Start.Equals(s)).Errorf("start position same")
+	assert(!t.End.Equals(e)).Errorf("end position same")
 }
