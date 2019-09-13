29a56d6989f124105e386ed7d083aa5caa3d8f40
Gregory Vincic <g@7de.se>
2019-09-13 21:52:18 +0200

    Test adjuster

---
+++ shape/adjust_test.go
@@ -0,0 +1,50 @@
+package shape
+
+import (
+	"testing"
+
+	"github.com/gregoryv/asserter"
+)
+
+func Test_Adjuster_At(t *testing.T) {
+	it := new_one_adjuster(t)
+
+	it.has_default_spacing()
+	it.takes_optional_spacing()
+	it.can_position_shapes()
+}
+
+func new_one_adjuster(t *testing.T) *one_adjuster {
+	return &one_adjuster{t, NewAdjuster(&Line{})}
+}
+
+type one_adjuster struct {
+	*testing.T
+	*Adjuster
+}
+
+func (t *one_adjuster) has_default_spacing() {
+	s := t.space([]int{})
+	if s != t.defaultSpace {
+		t.Error("No default spacing")
+	}
+}
+
+func (t *one_adjuster) takes_optional_spacing() {
+	s := t.space([]int{1})
+	if s != 1 {
+		t.Error("no optional spacing")
+	}
+}
+
+func (t *one_adjuster) can_position_shapes() {
+	t.At(1, 1)
+	shape := &Line{}
+	t.RightOf(shape)
+	assert := asserter.New(t)
+	x, _ := t.shape.Position()
+	assert(x == 30).Errorf("%v", x)
+	t.Below(shape)
+	_, y := t.shape.Position()
+	assert(y == 30).Errorf("%v", y)
+}
