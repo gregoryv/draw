84020b518d2f62995edb83f0505fdc1581edbca8
Gregory Vincic <g@7de.se>
2019-09-15 11:05:21 +0200

    Rename Adjust to Alignment

--- shape/align.go
+++ shape/align.go
@@ -1,12 +1,18 @@
 package shape
 
+// Aligner type aligns multiple shapes
 type Aligner int
 
+// HAlignCenter aligns shape[1:] to shape[0] center coordinates horizontally
 func (Aligner) HAlignCenter(shapes ...Shape) { hAlign(Center, shapes...) }
-func (Aligner) HAlignTop(shapes ...Shape)    { hAlign(Top, shapes...) }
+
+// HAlignTop aligns shape[1:] to shape[0] top coordinates horizontally
+func (Aligner) HAlignTop(shapes ...Shape) { hAlign(Top, shapes...) }
+
+// HAlignBottom aligns shape[1:] to shape[0] bottom coordinates horizontally
 func (Aligner) HAlignBottom(shapes ...Shape) { hAlign(Bottom, shapes...) }
 
-func hAlign(adjust Adjust, objects ...Shape) {
+func hAlign(adjust Alignment, objects ...Shape) {
 	first := objects[0]
 	_, y := first.Position()
 	for _, shape := range objects[1:] {
@@ -36,11 +42,16 @@ func hAlign(adjust Adjust, objects ...Shape) {
 	}
 }
 
+// VAlignCenter aligns shape[1:] to shape[0] center coordinates vertically
 func (Aligner) VAlignCenter(shapes ...Shape) { vAlign(Center, shapes...) }
-func (Aligner) VAlignLeft(shapes ...Shape)   { vAlign(Left, shapes...) }
-func (Aligner) VAlignRight(shapes ...Shape)  { vAlign(Right, shapes...) }
 
-func vAlign(adjust Adjust, objects ...Shape) {
+// VAlignLeft aligns shape[1:] to shape[0] left coordinates vertically
+func (Aligner) VAlignLeft(shapes ...Shape) { vAlign(Left, shapes...) }
+
+// VAlignRight aligns shape[1:] to shape[0] right coordinates vertically
+func (Aligner) VAlignRight(shapes ...Shape) { vAlign(Right, shapes...) }
+
+func vAlign(adjust Alignment, objects ...Shape) {
 	first := objects[0]
 	x, _ := first.Position()
 	for _, shape := range objects[1:] {
@@ -60,10 +71,10 @@ func vAlign(adjust Adjust, objects ...Shape) {
 	}
 }
 
-type Adjust int
+type Alignment int
 
 const (
-	Top Adjust = iota
+	Top Alignment = iota
 	Left
 	Right
 	Bottom
