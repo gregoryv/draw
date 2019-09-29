ea228d43307b8583b9af21fde4e07fca38716cda
Gregory Vincic <g@7de.se>
2019-09-29 06:56:44 +0200

    Refactor record_test.go

--- shape/record_test.go
+++ shape/record_test.go
@@ -11,7 +11,7 @@ func TestOneRecord(t *testing.T) {
 	rec := NewRecord("car")
 	rec.Fields = []string{"short", "longerField"}
 	rec.Methods = []string{"String", "Model"}
-	it := &OneRecord{t, rec}
+	it := NewOneRecord(t, rec)
 	it.HasFields()
 	it.HasMethods()
 	it.IsStyled()
@@ -20,25 +20,30 @@ func TestOneRecord(t *testing.T) {
 	it.RendersAsSvg()
 	it.CanMove()
 
-	it = &OneRecord{t, NewStructRecord(Record{})}
+	it = NewOneRecord(t, NewStructRecord(Record{}))
 	it.HasFields()
 	it.HasMethods()
 	it.CanHideFields()
 	it.CanHideMethods()
 
-	it = &OneRecord{t, NewInterfaceRecord((*Shape)(nil))}
+	it = NewOneRecord(t, NewInterfaceRecord((*Shape)(nil)))
 	it.IsMissingFields()
 	it.HasMethods()
 
-	it = &OneRecord{t, NewRecord("simple")}
+	it = NewOneRecord(t, NewRecord("simple"))
 	it.IsMissingFields()
 	it.IsStyled()
 	it.SHeightAdapts()
 	it.SWidthAdapts()
 }
 
+func NewOneRecord(t *testing.T, rec *Record) *OneRecord {
+	return &OneRecord{t, asserter.New(t), rec}
+}
+
 type OneRecord struct {
 	*testing.T
+	assert
 	*Record
 }
 
@@ -46,61 +51,49 @@ func (t *OneRecord) CanMove() {
 	t.Helper()
 	t.SetX(10)
 	dir := t.Direction()
-	assert := asserter.New(t)
-	assert(dir == LR).Error("Direction should always be LR for record")
+	t.assert(dir == LR).Error("Direction should always be LR for record")
 }
 
 func (t *OneRecord) HasFields() {
 	t.Helper()
-	assert := asserter.New(t)
-	assert(len(t.Fields) >= 0).Error("missing fields")
+	t.assert(len(t.Fields) >= 0).Error("missing fields")
 }
 
 func (t *OneRecord) CanHideFields() {
 	t.Helper()
-	assert := asserter.New(t)
 	t.HideFields()
-	assert(len(t.Fields) == 0).Error("fields not hidden")
+	t.assert(len(t.Fields) == 0).Error("fields not hidden")
 }
 
 func (t *OneRecord) IsMissingFields() {
 	t.Helper()
-	assert := asserter.New(t)
-	assert(len(t.Fields) == 0).Error("has fields")
+	t.assert(len(t.Fields) == 0).Error("has fields")
 }
 
 func (t *OneRecord) HasMethods() {
 	t.Helper()
-	assert := asserter.New(t)
-	assert(len(t.Methods) > 0).Error("missing methods")
+	t.assert(len(t.Methods) > 0).Error("missing methods")
 }
 
 func (t *OneRecord) CanHideMethods() {
 	t.Helper()
-	assert := asserter.New(t)
 	t.HideMethods()
-	assert(len(t.Methods) == 0).Error("methods not hidden")
+	t.assert(len(t.Methods) == 0).Error("methods not hidden")
 }
 
 func (t *OneRecord) SHeightAdapts() {
 	t.Helper()
-	assert := asserter.New(t)
-	assert(t.Height() > 0).Error("height did not adapt")
+	t.assert(t.Height() > 0).Error("height did not adapt")
 }
 
 func (t *OneRecord) SWidthAdapts() {
 	t.Helper()
-	assert := asserter.New(t)
-	assert(t.Width() > 0).Error("width did not adapt")
+	t.assert(t.Width() > 0).Error("width did not adapt")
 }
 
 func (t *OneRecord) IsStyled() {
 	t.SetFont(Font{Height: 9, Width: 7, LineHeight: 15})
 	t.SetTextPad(Padding{3, 3, 10, 2})
-}
-
-func (t *OneRecord) ReflectsAnInterface() {
-	t.Record = NewInterfaceRecord((*Shape)(nil))
 }
 
 func (t *OneRecord) RendersAsSvg() {
