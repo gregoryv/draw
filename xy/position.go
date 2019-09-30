be4531567ffac29be8c915eadd5d3efd380341d6
Gregory Vincic <g@7de.se>
2019-09-30 23:28:44 +0200

    Rm Within

--- xy/position.go
+++ xy/position.go
@@ -22,14 +22,3 @@ func (p Position) String() string {
 func (p Position) XYfloat64() (float64, float64) {
 	return float64(p.X), float64(p.Y)
 }
-
-func (p Position) Within(r Rect) bool {
-	if r.TopLeft.LeftOf(r.BottomRight) {
-		withinXspan := (r.TopLeft.X <= p.X && p.X <= r.BottomRight.X)
-		withinYspan := (r.TopLeft.Y <= p.Y && p.Y <= r.BottomRight.Y)
-		return withinXspan && withinYspan
-	}
-	withinXspan := (r.TopLeft.X >= p.X && p.X >= r.BottomRight.X)
-	withinYspan := (r.TopLeft.Y >= p.Y && p.Y >= r.BottomRight.Y)
-	return withinXspan && withinYspan
-}
