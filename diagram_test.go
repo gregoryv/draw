a165762ccda7db4c13dae820a0843cc459017649
Gregory Vincic <g@7de.se>
2019-12-15 09:16:19 +0100

    Tidy diagram_test.go

--- diagram_test.go
+++ diagram_test.go
@@ -8,43 +8,34 @@ import (
 	"github.com/gregoryv/go-design/shape"
 )
 
-func TestOneDiagram(t *testing.T) {
-	it := NewOneDiagram(t)
-	it.AdaptsInSize()
-	it.CanHaveFixedSize()
+func TestDiagram(t *testing.T) {
+	d := NewDiagram()
+	AdaptsInSize(t, &d)
+	CanHaveFixedSize(t, &d)
 }
 
-func NewOneDiagram(t *testing.T) *OneDiagram {
-	return &OneDiagram{T: t}
-}
-
-type OneDiagram struct {
-	*testing.T
-	Diagram
-}
-
-func (t *OneDiagram) AdaptsInSize() {
-	t.Log("Adapts in size")
+func AdaptsInSize(t *testing.T, d *Diagram) {
+	t.Helper()
 	l1 := shape.NewLine(0, 0, 100, 100)
 	l2 := shape.NewLine(0, 0, 100, 20)
-	t.Place(l1).At(0, 0)
-	t.Place(l2).Below(l1, 10)
-	w, h := t.AdaptSize()
+	d.Place(l1).At(0, 0)
+	d.Place(l2).Below(l1, 10)
+	w, h := d.AdaptSize()
 	assert := asserter.New(t)
 	assert(w == 100).Errorf("width did not adapt: %v", w)
 	assert(h == 130).Errorf("height did not adapt: %v", h)
 }
 
-func (t *OneDiagram) CanHaveFixedSize() {
-	t.Log("Can have fixed size")
-	t.Place(shape.NewLine(0, 0, 100, 100))
+func CanHaveFixedSize(t *testing.T, d *Diagram) {
+	t.Helper()
+	d.Place(shape.NewLine(0, 0, 100, 100))
 	adjusted := &bytes.Buffer{}
-	t.WriteSvg(adjusted)
+	d.WriteSvg(adjusted)
 
-	t.SetWidth(5)
-	t.SetHeight(10)
+	d.SetWidth(5)
+	d.SetHeight(10)
 	fixed := &bytes.Buffer{}
-	t.WriteSvg(fixed)
+	d.WriteSvg(fixed)
 
 	assert := asserter.New(t)
 	assert(adjusted.String() != fixed.String())
