074471251d3e12edaf1551219d814fa8fc7f846b
Gregory Vincic <g@7de.se>
2019-12-21 16:06:34 +0100

    Add package example

--- svg.go
+++ svg.go
@@ -1,6 +1,14 @@
 /*
 Package draw provides svg writing features.
 
+	s := NewSvg()
+	s.WriteSvg(os.Stdout)
+
+	<svg
+	  xmlns="http://www.w3.org/2000/svg"
+	  xmlns:xlink="http://www.w3.org/1999/xlink"
+	  width="100" height="100" font-family="Arial, Helvetica, sans-serif"></svg>
+
 */
 package draw
 
