afad73005ac938458c3c612fe8fa0a4916f7be4a
Gregory Vincic <g@7de.se>
2019-12-21 15:44:21 +0100

    Documenting TagWriter

--- tagprinter.go
+++ tagprinter.go
@@ -5,26 +5,20 @@ import (
 	"io"
 )
 
-// NewTagPrinter returns a printer nexus.
+// NewTagWriter returns a writer nexus. The referenced error is set by
+// all methods if an error occurs.
 func NewTagWriter(w io.Writer) (*TagWriter, *error) {
 	t := &TagWriter{w: w}
 	return t, &t.err
 }
 
-// TagPrinter is used to write out svg tags and if an error has
-// occured previously it's methods do nothing.
 type TagWriter struct {
 	w   io.Writer
 	err error
 }
 
-func (t *TagWriter) Printf(format string, args ...interface{}) {
-	if t.err != nil {
-		return
-	}
-	_, t.err = fmt.Fprintf(t.w, format, args...)
-}
-
+// Print prints arguments using the underlying writer. Does nothing if
+// TagWriter has failed.
 func (t *TagWriter) Print(args ...interface{}) {
 	if t.err != nil {
 		return
@@ -32,6 +26,17 @@ func (t *TagWriter) Print(args ...interface{}) {
 	_, t.err = fmt.Fprint(t.w, args...)
 }
 
+// Printf prints a formated string using the underlying writer. Does
+// nothing if TagWriter has failed.
+func (t *TagWriter) Printf(format string, args ...interface{}) {
+	if t.err != nil {
+		return
+	}
+	_, t.err = fmt.Fprintf(t.w, format, args...)
+}
+
+// Write writes the bytes using the underlying writer. Does nothing if
+// TagWriter has failed.
 func (t *TagWriter) Write(b []byte) (int, error) {
 	if t.err != nil {
 		return 0, t.err
