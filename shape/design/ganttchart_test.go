d2cde75ced45027aee21ddce5cc619d2451788fa
Gregory Vincic <g@7de.se>
2019-12-22 10:14:37 +0100

    Test GanttChart.WriteSvg

--- shape/design/ganttchart_test.go
+++ shape/design/ganttchart_test.go
@@ -1,6 +1,25 @@
 package design
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+
+	"github.com/gregoryv/golden"
+)
+
+func TestGanttChart_WriteSvg(t *testing.T) {
+	w := bytes.NewBufferString("")
+	var (
+		d = NewGanttChartFrom(30, 2019, 11, 11)
+	)
+	d.MarkDate(2019, 11, 20)
+	d.Add("Develop", 0, 10)
+	d.Add("Release", 10, 1).Red()
+	d.Add("Vacation", 14, 14).Blue()
+	d.SetCaption("Figure 1. Project estimated delivery")
+	d.WriteSvg(w)
+	golden.Assert(t, w.String())
+}
 
 func TestNewGanttChart(t *testing.T) {
 	NewGanttChartFrom(20, 2019, 10, 2)
