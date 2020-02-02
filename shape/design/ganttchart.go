e50641a0d35703b09b53fc63a8301be11f4da436
Gregory Vincic <g@7de.se>
2020-02-02 18:01:12 +0100

    Refactor Ganttchart.WriteSvg preparing for week view

--- shape/design/ganttchart.go
+++ shape/design/ganttchart.go
@@ -19,13 +19,14 @@ func NewGanttChart(from date.String, days int) *GanttChart {
 // start time. If no start is given, time.Now() is used.
 func newGanttChart(start time.Time, days int) *GanttChart {
 	d := &GanttChart{
-		Diagram: NewDiagram(),
-		start:   start,
-		days:    days,
-		tasks:   make([]*Task, 0),
-		padLeft: 16,
-		padTop:  10,
-		Mark:    time.Now(),
+		Diagram:  NewDiagram(),
+		start:    start,
+		days:     days,
+		tasks:    make([]*Task, 0),
+		padLeft:  16,
+		padTop:   10,
+		colSpace: 4,
+		Mark:     time.Now(),
 	}
 	return d
 }
@@ -37,6 +38,7 @@ type GanttChart struct {
 	tasks []*Task
 
 	padLeft, padTop int
+	colSpace        int // between day or week
 
 	// Set a marker at this date.
 	Mark time.Time
@@ -86,30 +88,38 @@ func (a *GanttAdjuster) After(parent *Task, days int) {
 	a.task.days = days
 }
 
-// NewTask returns a green task.
-func NewTask(txt string, offset, days int) *Task {
-	return &Task{
-		txt:    txt,
-		offset: offset,
-		days:   days,
-		class:  "span-green",
-	}
-}
-
-// Task is the colorized span of a gantt chart.
-type Task struct {
-	txt          string
-	offset, days int
-	class        string
-}
-
-// Red sets class of task to span-red
-func (t *Task) Red() *Task { t.class = "span-red"; return t }
-
-// Blue sets class of task to span-blue
-func (t *Task) Blue() *Task { t.class = "span-blue"; return t }
-
 func (d *GanttChart) WriteSvg(w io.Writer) error {
+	columns := d.addHeader()
+	bars := make([]*shape.Rect, len(d.tasks))
+	start := d.padLeft + d.taskWidth()
+	lineHeight := d.Diagram.Font.LineHeight
+	headerHeight := d.padTop + lineHeight*3
+	for i, t := range d.tasks {
+		rect := shape.NewRect("")
+		rect.SetHeight(d.Diagram.Font.Height)
+		rect.SetClass(t.class)
+		bars[i] = rect
+		d.drawTask(i, t)
+		y := i*lineHeight + headerHeight
+		d.Diagram.Place(rect).At(start, y)
+	}
+
+	for i := 0; i < d.days; i++ {
+		dayWidth := columns[i].Width() + d.colSpace
+		for j, t := range d.tasks {
+			switch {
+			case i == t.offset:
+				bars[j].SetX(start + i*dayWidth)
+			case i > t.offset && i < t.offset+t.days:
+				bars[j].SetWidth(bars[j].Width() + dayWidth)
+			}
+		}
+	}
+
+	return d.Diagram.WriteSvg(w)
+}
+
+func (d *GanttChart) addHeader() []*shape.Label {
 	now := d.start
 	year := shape.NewLabel(fmt.Sprintf("%v", now.Year()))
 	d.Diagram.Place(year).At(d.padLeft, d.padTop)
@@ -124,20 +134,20 @@ func (d *GanttChart) WriteSvg(w io.Writer) error {
 		if now.Weekday() == time.Saturday {
 			bg := shape.NewRect("")
 			bg.SetClass("weekend")
-			bg.SetWidth(col.Width()*2 + 8)
-			bg.SetHeight(len(d.tasks)*col.Font.LineHeight + d.padTop + d.Diagram.Font.LineHeight)
-			d.Diagram.Place(bg).RightOf(lastDay, 4)
-			shape.Move(bg, -2, 4)
+			bg.SetWidth((col.Width() + d.colSpace) * 2)
+			bg.SetHeight(len(d.tasks)*col.Font.LineHeight + d.padTop + d.Diagram.Font.LineHeight + d.colSpace)
+			d.Diagram.Place(bg).RightOf(lastDay, d.colSpace)
+			shape.Move(bg, -d.colSpace/2, d.colSpace)
 		}
 		if i == 0 {
-			d.Diagram.Place(col).Below(year, 4)
+			d.Diagram.Place(col).Below(year, d.colSpace)
 			col.SetX(offset)
 		} else {
-			d.Diagram.Place(col).RightOf(lastDay, 4)
+			d.Diagram.Place(col).RightOf(lastDay, d.colSpace)
 		}
 		if day == 1 {
 			label := shape.NewLabel(now.Month().String())
-			d.Diagram.Place(label).Above(col, 4)
+			d.Diagram.Place(label).Above(col, d.colSpace)
 		}
 		if d.isToday(i) {
 			x, y := col.Position()
@@ -147,32 +157,16 @@ func (d *GanttChart) WriteSvg(w io.Writer) error {
 		lastDay = col
 		now = now.AddDate(0, 0, 1)
 	}
+	return columns
+}
 
-	var lastTask *shape.Label
-	for i, t := range d.tasks {
-		label := shape.NewLabel(t.txt)
-		if i == 0 {
-			d.Diagram.Place(label).Below(lastDay, 4)
-			d.VAlignLeft(year, label)
-		} else {
-			d.Diagram.Place(label).Below(lastTask, 4)
-		}
-		lastTask = label
-
-		rect := shape.NewRect("")
-		col := columns[t.offset]
-		var w int
-		for i := t.offset; i < t.offset+t.days; i++ {
-			w += columns[i].Width() + 4
-		}
-		rect.SetWidth(w - 4)
-		rect.SetHeight(d.Diagram.Font.Height)
-		rect.SetClass(t.class)
-
-		d.Diagram.Place(rect).Below(col, 4)
-		d.HAlignCenter(label, rect)
-	}
-	return d.Diagram.WriteSvg(w)
+func (d *GanttChart) drawTask(i int, t *Task) {
+	label := shape.NewLabel(t.txt)
+	lineHeight := d.Diagram.Font.LineHeight
+	headerHeight := d.padTop + lineHeight*3
+	x := d.padLeft
+	y := i*lineHeight + headerHeight - lineHeight/3
+	d.Diagram.Place(label).At(x, y)
 }
 
 func newCol(day int) *shape.Label {
@@ -195,3 +189,26 @@ func (d *GanttChart) taskWidth() int {
 	}
 	return x + d.padLeft
 }
+
+// NewTask returns a green task.
+func NewTask(txt string, offset, days int) *Task {
+	return &Task{
+		txt:    txt,
+		offset: offset,
+		days:   days,
+		class:  "span-green",
+	}
+}
+
+// Task is the colorized span of a gantt chart.
+type Task struct {
+	txt          string
+	offset, days int
+	class        string
+}
+
+// Red sets class of task to span-red
+func (t *Task) Red() *Task { t.class = "span-red"; return t }
+
+// Blue sets class of task to span-blue
+func (t *Task) Blue() *Task { t.class = "span-blue"; return t }
