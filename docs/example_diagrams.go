40942fbe43ee1f4b5a1bd2031e03d07bf8dac502
Gregory Vincic <g@7de.se>
2020-11-13 05:51:34 +0100

    Show sequence diagram groups in docs

--- docs/example_diagrams.go
+++ docs/example_diagrams.go
@@ -95,12 +95,15 @@ func ExampleSequenceDiagram() *design.SequenceDiagram {
 		cli = d.AddStruct(app.Client{})
 		srv = d.AddStruct(app.Server{})
 		db  = d.AddStruct(sql.DB{})
+		sqs = d.Add("aws.SQS")
 	)
+	d.Group(srv, sqs, "Private RPC using Gob encoding", "red")
 	d.Link(cli, srv, "connect()")
 	d.Link(srv, db, "SELECT").Class = "highlight"
 	d.Link(db, srv, "Rows")
 	d.Link(srv, srv, "Transform to view model").Class = "highlight"
 	d.Link(srv, cli, "Send HTML")
+	d.Link(srv, sqs, "Publish event")
 	return d
 }
 
