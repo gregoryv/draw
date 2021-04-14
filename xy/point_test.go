65b0af032ef0384bcaef77678f9bdd14b9dac992
Gregory Vincic <g@7de.se>
2021-04-14 06:38:24 +0200

    Rename type one_position -> OnePoint

--- xy/point_test.go
+++ xy/point_test.go
@@ -21,8 +21,8 @@ func TestPoint_Distance(t *testing.T) {
 
 }
 
-func Test_one_position(t *testing.T) {
-	it := one_position{t, Point{10, 10}}
+func Test_OnePoint(t *testing.T) {
+	it := OnePoint{t, Point{10, 10}}
 	q := Point{10, 10}
 	it.is_not_left_of(q)
 	it.is_not_right_of(q)
@@ -33,54 +33,54 @@ func Test_one_position(t *testing.T) {
 	it.is_stringable()
 }
 
-type one_position struct {
+type OnePoint struct {
 	*testing.T
 	Point
 }
 
-func (t one_position) is_left_of(q Point) {
+func (t OnePoint) is_left_of(q Point) {
 	t.Helper()
 	if !t.LeftOf(q) {
 		t.Errorf("%v should be left of %v", t.Point, q)
 	}
 }
 
-func (t one_position) is_not_left_of(q Point) {
+func (t OnePoint) is_not_left_of(q Point) {
 	t.Helper()
 	if t.LeftOf(q) {
 		t.Errorf("%v should not be left of %v", t.Point, q)
 	}
 }
 
-func (t one_position) is_not_right_of(q Point) {
+func (t OnePoint) is_not_right_of(q Point) {
 	t.Helper()
 	if t.RightOf(q) {
 		t.Errorf("%v should not be right of %v", t.Point, q)
 	}
 }
 
-func (t one_position) is_not_above(q Point) {
+func (t OnePoint) is_not_above(q Point) {
 	t.Helper()
 	if t.Above(q) {
 		t.Errorf("%v should not be above %v", t.Point, q)
 	}
 }
 
-func (t one_position) is_not_below(q Point) {
+func (t OnePoint) is_not_below(q Point) {
 	t.Helper()
 	if t.Below(q) {
 		t.Errorf("%v should not be below %v", t.Point, q)
 	}
 }
 
-func (t one_position) is_same_as(q Point) {
+func (t OnePoint) is_same_as(q Point) {
 	t.Helper()
 	if !t.Equals(q) {
 		t.Errorf("%v should be equal to %v", t.Point, q)
 	}
 }
 
-func (t one_position) has_quick_access_to_coordinates() {
+func (t OnePoint) has_quick_access_to_coordinates() {
 	t.Helper()
 	x, y := t.XY()
 	if x != t.X || y != t.Y {
@@ -88,7 +88,7 @@ func (t one_position) has_quick_access_to_coordinates() {
 	}
 }
 
-func (t one_position) is_stringable() {
+func (t OnePoint) is_stringable() {
 	t.Helper()
 	str := t.String()
 	if str == "" {
