d038458498509a6a4a81a5bcc6f4f26602c833e8
Gregory Vincic <g@7de.se>
2021-10-23 08:32:53 +0200

    Identify assets

--- design/threatmodel_test.go
+++ design/threatmodel_test.go
@@ -3,6 +3,7 @@ package design_test
 import (
 	"math"
 
+	"github.com/gregoryv/draw"
 	"github.com/gregoryv/draw/design"
 	"github.com/gregoryv/draw/shape"
 )
@@ -17,40 +18,54 @@ func Example_threatModel() {
 		f = Entity("Customer\nService")
 	)
 	d.Style.Spacing = 60
-	/* how about
-		 d.Layout(
-		`
-	      4
-	1 2 3
-	      5`*/
-
+	// Identify components
 	d.Place(a).At(20, 100)
 	d.Place(b, c, e).RightOf(a)
 	shape.Move(e, 0, -50)
 	d.Place(f).Below(e)
 
+	// Add data flows
 	d.LinkAll(a, b, c)
 	d.Link(c, e)
 	d.Link(c, f)
 
-	b1 := Boundary(b, c)
+	// Identify trust boundaries
+	b1 := Boundary(b, c, 20, -3)
 	d.Place(b1)
+	d.Place(shape.NewLabel("internet")).At(40, 40)
 
-	d.SetCaption("Figure 3. Threat model diagram")
+	// Show your assets
+	creds := Asset("creds")
+	d.Place(creds).Above(c)
+	shape.Move(creds, 20, 80)
+
+	pii := Asset("PII") // personally identifable information (PII)
+	d.Place(pii).Below(f)
+	shape.Move(pii, 20, -65)
+
+	d.SetCaption("Figure 3. Customer profile page threat model")
 	d.SaveAs("img/threatmodel_example.svg")
 	// output:
 }
 
+func Asset(text string) shape.Shape {
+	a := shape.NewRect(text)
+	a.SetClass("asset")
+	draw.DefaultClassAttributes["asset"] = `stroke="orange" fill="orange"`
+	draw.DefaultClassAttributes["asset-title"] = `font-family="Arial,Helvetica,sans-serif"`
+	return a
+}
+
 // Boundary
-func Boundary(s1, s2 shape.Shape) shape.Shape {
+func Boundary(s1, s2 shape.Shape, extra, slant int) shape.Shape {
 	x1, y1 := s1.Position()
 	x2, y2 := s2.Position()
 
 	xd := x2 - (x1 + s1.Width())
 	x := x1 + s1.Width() + xd/2
 
-	e := 10 // extra
-	s := 3  // slant, a bit of an angle
+	e := extra // extra
+	s := slant // slant, a bit of an angle
 	l := shape.NewLine(x+s, y1-e, x-s, y2+s2.Height()+e)
 	l.SetClass("boundary")
 	return l
