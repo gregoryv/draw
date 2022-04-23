f09a874286a5cf0f0e56abed044a23f78c778125
Gregory Vincic <g@7de.se>
2022-04-23 10:29:44 +0200

    Shorten package docs
    Examples contain the same information.

--- svg.go
+++ svg.go
@@ -1,15 +1,4 @@
-/*
-Package draw provides SVG writing features.
-
-	s := NewSVG()
-	s.WriteSVG(os.Stdout)
-
-	<svg
-	  xmlns="http://www.w3.org/2000/svg"
-	  xmlns:xlink="http://www.w3.org/1999/xlink"
-	  width="100" height="100" font-family="Arial, Helvetica, sans-serif"></svg>
-
-*/
+// Package draw provides SVG writing features.
 package draw
 
 import (
